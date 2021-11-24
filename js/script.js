c0129d21bee5e5f6a548a97ee1e308e9e18465c0
rafapaolino <rafaelpp3@al.insper.edu.br>
2021-11-24 20:51:38 -0300

    kljh

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
+    
     hoje = document.querySelector('#data')
 
-    dia  = data.getDate().toString(),
-    diaF = (dia.length == 1) ? '0'+dia : dia,
-    mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
-    mesF = (mes.length == 1) ? '0'+mes : mes,
-    anoF = data.getFullYear();
-    data = diaF+"/"+mesF+"/"+anoF;
+    1
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    
+    
 
-    hoje.innerHTML = data
+    console.log(date)
+
+    hoje.innerHTML = date
     
     
 })
