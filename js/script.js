765950b25c7ad82db5fd6a36f78926f3d1f4a6a6
luizaehrenberger <luizaehrenberger@gmail.com>
2021-11-24 22:07:32 -0300

    ksd

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-    
     hoje = document.querySelector('#data')
 
     var today = new Date();
-
-
     var dia = today.getDate();
     var nmes = today.getMonth()+1
+
     dicimes = {
         1: 'Janeiro', 2: 'Fevereiro', 3: 'Março', 4: 'Abril', 5: 'Maio', 6: 'Junho', 7: 'Julho', 8: 'Agosto', 9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'
     }
@@ -15,8 +13,10 @@ document.addEventListener('DOMContentLoaded', function() {
     date = dia + " " +  mes ;
     console.log(date)
 
-
-
     hoje.innerHTML = date
     
+    hoje.addEventListener('click', function(event){
+        window.location="/tela_mes.html";
+    })
+
 })
