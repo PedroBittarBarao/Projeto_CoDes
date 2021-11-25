81b8be1adb9ef1c1d0f8a6bdf2379d42ce4095bd
Rafael Paolino <rafaelpp3@al.insper.edu.br>
2021-11-25 08:13:16 -0300

    v

---
+++ js/detalhes_financias.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    data = document.querySelector('#data').innerHTML
+
+    var today = new Date();
+    var dia = today.getDate();
+    var nmes = today.getMonth()+1;
+    
+    ld = data.split
+
+
+})
