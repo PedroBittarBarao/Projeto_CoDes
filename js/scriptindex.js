7e86f04fd26cab68099eb3f6e4a2f36acd97ff49
luizaehrenberger <luizaehrenberger@gmail.com>
2021-11-25 08:57:22 -0300

    uy

--- js/scriptindex.js
+++ js/scriptindex.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
         1: 'Janeiro', 2: 'Fevereiro', 3: 'Março', 4: 'Abril', 5: 'Maio', 6: 'Junho', 7: 'Julho', 8: 'Agosto', 9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'
     }
     mess = dicimes[nmes];
-    date = dia + ' ' + mess;
+    date = dia + ' de ' + mess;
     
     hoje.innerHTML = date;
     
