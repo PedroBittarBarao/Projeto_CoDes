78cd011b3224749a35077daba8682fdd7b474bdf
Rafael Paolino <rafaelpp3@al.insper.edu.br>
2021-11-25 08:44:36 -0300

    pendente funcional brabo

--- js/detalhes_financias.js
+++ js/detalhes_financias.js
@@ -6,7 +6,28 @@ document.addEventListener('DOMContentLoaded', function() {
     var dia = today.getDate();
     var nmes = today.getMonth()+1;
     
-    ld = data.split
+    ld = data.split('/')
+    console.log(ld)
+
+    diapag = parseInt(ld[0])
+    mespag = parseInt(ld[1])
+    console.log(diapag)
+    console.log(mespag)
+
+    console.log(dia)
+    console.log(nmes)
+
+    if(mespag <= nmes) {
+        console.log('if 1')
+        if(diapag < dia) {
+            console.log('if 2')
+            p = document.createElement('p')
+            p.className = 'pendente'
+            p.innerHTML = 'pendente'
+            main = document.querySelector('header')
+            main.prepend(p)
+        }
+    }
 
 
 })
