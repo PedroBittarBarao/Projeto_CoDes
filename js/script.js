92caa29d65154cb8bc55c167f77341eb67bef7d7
luizaehrenberger <luizaehrenberger@gmail.com>
2021-11-24 21:06:10 -0300

    arrumei

--- js/script.js
+++ js/script.js
@@ -3,10 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var today = new Date();
 
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
+    var dia = today.getDate();
+    var nmes = today.getMonth()+1
+    dicimes = {
+        1: 'Janeiro', 2: 'Fevereiro', 3: 'Março', 4: 'Abril', 5: 'Maio', 6: 'Junho', 7: 'Julho', 8: 'Agosto', 9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'
+    }
+    mes = dicimes[nmes];
+    date = dia + mes ;
     console.log(date)
+
     
+
     hoje.innerHTML = date
     
 })
