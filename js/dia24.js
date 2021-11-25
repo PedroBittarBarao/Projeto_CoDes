f64a39ff7b0b8d0baed38842e5b91457491b8036
rafapaolino <rafaelpp3@al.insper.edu.br>
2021-11-24 22:58:04 -0300

    dnv

---
+++ js/dia24.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    meshtml = document.querySelector('#mes')
+
+    var today = new Date();
+    var dia = today.getDate();
+    var nmes = today.getMonth()+1;
+
+    dicimes = {
+        1: 'Janeiro', 2: 'Fevereiro', 3: 'Março', 4: 'Abril', 5: 'Maio', 6: 'Junho', 7: 'Julho', 8: 'Agosto', 9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'
+    }
+    mes = dicimes[nmes];
+    
+    meshtml.innerHTML = mes;
+    
+})
